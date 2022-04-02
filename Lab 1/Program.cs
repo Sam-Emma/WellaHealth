80e27db86f0fce224880841a8f84265a7a842039
Samuel Emmanuel <58962390+Sam-Emma@users.noreply.github.com>
2022-04-02 02:36:28 -1200

    Add files via upload

---
+++ Lab 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Samuel Emmanuel";
+            string track = "Backend";
+            Console.WriteLine("Hello " + name +", I am in "+track + " of Wella Schools");
+            Console.WriteLine("I\"m learning Backend development so I build backend solutions for machine learning");
+        }
+    }
+}
